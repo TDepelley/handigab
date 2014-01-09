20777ec9ea8c0b825f814258aac16000d5c0f244
tuan0015 <anh-tuan.nguyen@ecole.ensicaen.fr>
2014-01-09 13:34:04 +0100

    salut c'est reremoi

--- HandiGab_porteur/src/com/example/handigab_porteur/CardsActivity.java
+++ HandiGab_porteur/src/com/example/handigab_porteur/CardsActivity.java
@@ -1,7 +1,5 @@
 package com.example.handigab_porteur;
 
-import com.example.handigab_porteur.bluetooth.BluetoothClientService;
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -10,16 +8,13 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.Space;
+import android.widget.TextView;
 
 public class CardsActivity extends Activity {
 
@@ -29,54 +24,55 @@ public class CardsActivity extends Activity {
 	ArrayList<String> card1; 
 	ArrayList<String> card2; 
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cards);
-		
+
 		nbCard=2;
 		card1 = new ArrayList<String>(); 
-		
+
 		String cardNumber="1234567890123456";
 		String date="0115";
 		String lastName="DUPONT";
 		String firstName="Alice";
 		String bankName="Societe Generale";
 		String cardType="Visa";
-		
+
 		cardPreferences(PREFS_CARD1,card1,cardNumber,date,lastName,firstName,bankName,cardType);
-		
+
 		card2 = new ArrayList<String>();	
 		//cardInfo("card1",card1);
-		
+
 		cardNumber="0987654321098765";
 		date="0116";
 		lastName="DUPONT";
 		firstName="Alice";
 		bankName="Le Credit Lyonnais";
 		cardType="Mastercard";
-		
+
 		//cardInfo("card2",card2);
 		cardPreferences(PREFS_CARD2,card2,cardNumber,date,lastName,firstName,bankName,cardType);
-		
+
 		createButton(1,PREFS_CARD1,card1);
 		createButton(2,PREFS_CARD2,card2);
-		
 
-		
+		TextView tv = (TextView)this.findViewById(R.id.infoClient);
+		tv.setText("Bonjour " + firstName +" "+lastName+"!");
 	}
-	
+
 	public void createButton(int i,String pref, ArrayList<String> card){
+		Space sp = new Space(this);
+		sp.setPadding(0, 10, 0, 10);
 		Button btn = new Button(this);
 		btn.setId(i);
 		btn.setText("N¡ "+card.get(0));
-		
+
 		btn.setBackgroundResource(R.drawable.button_normal_xml);
 		btn.setTag(pref);
-		
+
 		btn.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -86,11 +82,12 @@ public class CardsActivity extends Activity {
 				startActivity(intent);
 			}
 		});
-		
+
 		LinearLayout l = (LinearLayout)this.findViewById(R.id.cardsLayout);
+		l.addView(sp);
 		l.addView(btn);
 	}
-	
+
 	public void cardInfo(String info,ArrayList<String> card){
 		card = new ArrayList<String>(); 	
 		try{
@@ -108,38 +105,28 @@ public class CardsActivity extends Activity {
 			System.out.println(e.toString());
 		}
 	}
-	
+
 
 	public void cardPreferences(String prefs_card, ArrayList<String> card, String cardNumber,
 			String date,String lastName, String firstName,String bankName,String cardType){	
 		SharedPreferences settings = getSharedPreferences(prefs_card, 0);
 		SharedPreferences.Editor editor = settings.edit();	
-		
-		
-		
+
 		card.add(cardNumber);
 		card.add(date);
 		card.add(lastName);
 		card.add(firstName);
 		card.add(bankName);
 		card.add(cardType);
-	
+
 		editor.putString("cardNumber",card.get(0));
 		editor.putString("date",card.get(1));
 		editor.putString("lastName",card.get(2));
 		editor.putString("firstName",card.get(3));
 		editor.putString("bankName",card.get(4));
 		editor.putString("cardType",card.get(5));
-		
+
 		editor.commit();
 	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.cards, menu);
-		return true;
-	}
 
-	
 }
